3683d0455b5b53d746db330d4a0d3711e4ddadb3
d.georgiev.91 <d.georgiev.91@gmail.com>
2013-08-15 11:52:30 +0300

    revert

--- WebChat/WebChat.Api/App_Start/WebApiConfig.cs
+++ WebChat/WebChat.Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebChat.Api
 {
@@ -23,6 +24,7 @@ namespace WebChat.Api
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
         }
     }
 }
